68769680fd8090f7f718e4e47316fab8a24e406d
Liauren Permata Sari <liaurenpermata@gmail.com>
2022-09-12 20:29:23 +0700

    day 1

---
+++ day-1.js
@@ -0,0 +1,38 @@
+// comments can make code readable
+
+// Welcome to 30DaysOfJavascript
+
+/* Comments can make code ReadableByteStreamController,
+easy to reuse and informative */
+
+'string'
+true
+let undefine;
+console.log(undefine)
+
+let emptynull = null
+
+console.log(typeof 'lia')
+console.log(typeof 5)
+console.log(typeof false)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+let firstName;
+let lastName;
+let maritalStatus;
+let country;
+let age;
+
+let firstName1 = 'lia';
+let lastName1 = 'liaaa';
+let maritalStatus1 = 'single';
+let country1 = 'indonesia';
+let age1 = 22;
+
+let myAge = 25;
+let yourAge = 30;
+
+console.log('I am ' + myAge + ' years old.')
+console.log('you are ' + yourAge + ' years old.')
